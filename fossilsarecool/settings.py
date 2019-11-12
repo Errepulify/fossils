9288c5565377c9fdd8776d9facb3767f321bc4d6
Ubuntu <ubuntu@ip-172-31-92-120.ec2.internal>
2019-11-12 18:28:06 +0000

    Added the temapltes dirs

--- fossilsarecool/settings.py
+++ fossilsarecool/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fossilsarecool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'shop','templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
